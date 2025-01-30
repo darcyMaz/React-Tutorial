d85164169104469060a7599fcdd5a6775c0333e6
Darcy Mazloum <damazinator11@gmail.com>
2025-01-30 15:54:02 -0500

    End of lesson 6

--- dojo-blog/src/App.js
+++ dojo-blog/src/App.js
@@ -1,21 +1,13 @@
-import './App.css';
+import Navbar from './Navbar';
+import Home from './Home'
 
 function App() {
-  
-  const title = "Welcome to the new blog folks!";
-  const likes = 50;
-  const link = "https://www.google.com";
 
   return (
     <div className="App">
+      <Navbar />
       <div className="content"> 
-        <h1> {title} </h1>
-        <p> Liked {likes} times! </p>
-
-        <p> {"What's up guys!"} </p>
-        <p> {Math.random() * 10} </p>
-
-        <a href={link}> Go to Google! </a>
+        <Home />
       </div>
     </div>
   );
