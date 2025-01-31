645c63426adb85ea1230273a201defc46dc0b676
Darcy Mazloum <damazinator11@gmail.com>
2025-01-31 17:03:14 -0500

    End of lesson 7
    Events and anonymous functions

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log("Hello, World!", e);
+    };
+
+    const handleClick1 = (name, e) => {
+        console.log('Hello ' + name, e.target);
+    };
+
     return (
         <div className="home">
             <h2> Homepage </h2>
+            <button onClick={handleClick}> Click me! </button>
+            <button onClick={(e) => {
+                handleClick1('insert name here.', e)
+            }}> Using an anonymous function! </button>
         </div>
     )
 }
--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -4,11 +4,9 @@ const Navbar = () => {
             <h1> The Dojo Blog </h1>
             <div className="links"> 
                 <a href = "/"> Home </a>
-                <a href = "/create" style={{
-                    color: "white",
-                    backgroundColor: '#f1356d',
-                    borderRadius: '8px'
-                }}> New Blog </a>
+                <a href = "/create" className="newblogbutton"> 
+                  New Blog 
+                </a>
             </div>
         </nav>
     );
