a02962bbb69636df1b6220fee5ffc924fc7172eb
Darcy Mazloum <damazinator11@gmail.com>
2025-02-03 12:48:38 -0500

    End of lesson 11
    Made our first components, learned about props and hooks.

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,22 +1,32 @@
+import {useState} from 'react'
+import BlogList from './BlogList';
+
 const Home = () => {
 
-    const handleClick = (e) => {
-        console.log("Hello, World!", e);
-    };
+    // Initial value is an array of blogs.
+    const [blogs, setBlogs] = useState([
+        {title: 'We have made a website!', body: 'lorem ipsum...', author: 'Darcy Mazloum', id:1},
+        {title: 'The second author on our new website!', body: 'lorem ipsum...', author: 'Andrzej Fedorowicz', id:2},
+        {title: 'An update about my life.', body: 'lorem ipsum...', author: 'Merill Bobotis', id:3},
+    ])
 
-    const handleClick1 = (name, e) => {
-        console.log('Hello ' + name, e.target);
-    };
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id)
+        setBlogs(newBlogs)
+    }
 
     return (
         <div className="home">
-            <h2> Homepage </h2>
-            <button onClick={handleClick}> Click me! </button>
-            <button onClick={(e) => {
-                handleClick1('insert name here.', e)
-            }}> Using an anonymous function! </button>
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <BlogList blogs={blogs.filter( (blog) => blog.author === 'Andrzej Fedorowicz')} title="Mario's Blogs" handleDelete={handleDelete} />
+            
         </div>
     )
 }
 
 export default Home;
+
+// State of the page and its components can be accessed.
+//   useState hook!
+//   We must change the state to change things on the page.
+// Props: They let us pass data from a parent component to a child component.
