134e7ee64842e2522fa877e5425c663adf4b7f27
Darcy Mazloum <damazinator11@gmail.com>
2025-02-03 15:33:22 -0500

    End of lesson 17 +
    Setting up a JSON server and fetching data. Buttons that hide elements and then brings them all back from the server. It would probably be better to keep those local and simply change their visibility to reduce server calls. Unimportant for tiny project like this.

--- dojo-blog/src/BlogList.js
+++ dojo-blog/src/BlogList.js
@@ -1,18 +1,22 @@
 const BlogList = (props) => {
-    const blogs = props.blogs
-    const title = props.title
-    const handleDelete = props.handleDelete
+const blogs = props.blogs 
+const title = props.title
+const handleHide = props.handleHide
+const handleShowAll = props.handleShowAll
+
+
 
     return ( 
 
 
         <div className="blog-list">
             <h2> {title} </h2>
+            <button className="visibility-button" onClick={() => handleShowAll()}>  Show all blogs.  </button>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2> {blog.title} </h2>
                     <p> Written by: {blog.author} </p>
-                    <button className="delete-button" onClick={() => handleDelete(blog.id)}> Delete Blog </button>
+                    <button className="visibility-button" onClick={() => handleHide(blog.id)}> Hide Blog </button>
                 </div>
             ))}
         </div>
--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -3,46 +3,46 @@ import BlogList from './BlogList';
 
 const Home = () => {
 
-    // Initial value is an array of blogs.
-    const [blogs, setBlogs] = useState([
-        {title: 'We have made a website!', body: 'lorem ipsum...', author: 'Darcy Mazloum', id:1},
-        {title: 'The second author on our new website!', body: 'lorem ipsum...', author: 'Andrzej Fedorowicz', id:2},
-        {title: 'An update about my life.', body: 'lorem ipsum...', author: 'Merill Bobotis', id:3},
-    ])
+    const [blogs, setBlogs] = useState(null)
 
-    const [name, setName] = useState('mario')
-
-    const handleDelete = (id) => {
+    const handleHide = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id)
         setBlogs(newBlogs)
     }
+    const handleDelete = (id) => {
+        
+    }
+    const handleShowAll = () => {
+        const newBlogs = getAllBlogs()
+        setBlogs(newBlogs)
+    }
+    const getAllBlogs = () => 
+    {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json() // Passes response object into JSON object
+        })
+        .then((data) => {
+            // data is the returned data to be used!
+            setBlogs(data)
+            // console.log(data)
+        })
+    }
+    
 
-    // This runs on first load and every time the data changes.
+
+
     useEffect( () => {
-        console.log("useEffect has run.")
-        // useEffect can access the state too
-        console.log(blogs)
-        console.log(name)
-        // You should not change the state with useEffect because it will rerender, change the state, rerender, loop!
-    }, [name])
-
-    // Dependency array will allow us to have funcs that dont run every change.
-    // So the dependency array has all the values that when changed, make useEffect run.
+        getAllBlogs()
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
-            
-            <button onClick={() => setName('Luigi')}> Change Name </button>
-
-            <BlogList blogs={blogs.filter( (blog) => blog.author === 'Andrzej Fedorowicz')} title="Mario's Blogs" handleDelete={handleDelete} />
+            {
+                blogs && <BlogList blogs={blogs} title="All Blogs" handleHide={handleHide} handleShowAll={handleShowAll} />
+            }
         </div>
     )
 }
 
 export default Home;
-
-// State of the page and its components can be accessed.
-//   useState hook!
-//   We must change the state to change things on the page.
-// Props: They let us pass data from a parent component to a child component.
